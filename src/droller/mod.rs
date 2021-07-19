15cf2e9820622f830bdc2d633739cc1b1f688cfc
Nicolas Frankel <nicolas@frankel.ch>
2021-07-19 09:37:40 +0200

    Refactor code into module

---
+++ src/droller/mod.rs
@@ -0,0 +1,12 @@
+use rand::Rng;
+
+pub struct Die {
+    pub faces: u8,
+}
+
+impl Die {
+    pub fn roll(self) -> u8 {
+        let mut rng = rand::thread_rng();
+        rng.gen_range(1..=self.faces)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
-use rand::Rng;
+mod droller;
 
-struct Die {
-    faces: u8,
-}
-
-impl Die {
-    fn roll(self) -> u8 {
-        let mut rng = rand::thread_rng();
-        rng.gen_range(1..=self.faces)
-    }
-}
+use crate::droller::Die;
 
 fn main() {
     let d6 = Die { faces: 6 };
